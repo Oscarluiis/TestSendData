f24c4b0a0b5b340786410698ad0771347f2b4d1d
Oscar Luis Sanchez <tinocoscar11@unitec.edu>
2024-02-04 21:11:10 -0600

    Add some radio buttons

--- send-data/src/views/DataTest.js
+++ send-data/src/views/DataTest.js
@@ -5,6 +5,7 @@ import { getFirestore, addDoc, collection } from "firebase/firestore";
 export const DataTest = () => {
   const [inputValue1, setInputValue1] = useState("");
   const [inputValue2, setInputValue2] = useState("");
+  const [inputValue3, setInputValue3] = useState("");
 
   const db = getFirestore();
 
@@ -12,13 +13,14 @@ export const DataTest = () => {
     const docRef = await addDoc(collection(db, "myCollectionCopeco"), {
       field1: inputValue1,
       field2: inputValue2,
+      field3: inputValue3,
     });
     alert("Document written to Database");
   };
 
   return (
-  <>
-  <h1>Probando Enviar</h1>
+    <>
+      <h1>Probando Enviar</h1>
       <input
         type="text"
         value={inputValue1}
@@ -29,7 +31,21 @@ export const DataTest = () => {
         value={inputValue2}
         onChange={(e) => setInputValue2(e.target.value)}
       />
+
+      <h3>Choose an Option:</h3>
+
+      <label>
+        <input type="radio" name="option" value="option1" onChange={(e) => setInputValue3(e.target.value)}/> Option 1
+      </label>
+
+      <label>
+        <input type="radio" name="option" value="option2" onChange={(e) => setInputValue3(e.target.value)}/> Option 2
+      </label>
+
+      <label>
+        <input type="radio" name="option" value="option3" onChange={(e) => setInputValue3(e.target.value)}/> Option 3
+      </label>
       <button onClick={saveDataToFirestore}>Save to Firestore</button>
-      </>  
+    </>
   );
 };
